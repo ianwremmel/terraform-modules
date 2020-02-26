30546326d51b168725f4b979357ede245b831abd
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-02-26 08:21:26 -0800

    fix(humans): use correct resource path

--- aws/humans/output.tf
+++ aws/humans/output.tf
@@ -3,7 +3,7 @@ output "humans" {
     for human in var.humans :
     human => {
       arn                = aws_iam_user.this[human].arn
-      encrypted_password = aws_iam_user_login_profile[human].encrypted_password
+      encrypted_password = aws_iam_user_login_profile.this[human].encrypted_password
       name               = human
       tags               = aws_iam_user.this[human].tags
       unique_id          = aws_iam_user.this[human].unique_id
