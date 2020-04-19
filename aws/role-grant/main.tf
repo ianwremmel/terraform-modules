c51407764dafd0c21d5dac46d09d333c629d2f64
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-04-19 13:18:00 -0700

    feat(aws): allow passing a full policy json to role-grant

--- aws/role-grant/main.tf
+++ aws/role-grant/main.tf
@@ -1,10 +1,11 @@
 locals {
-  count  = local.enable ? 1 : 0
-  enable = var.enable && length(var.statements) > 0
+  count       = local.enable ? 1 : 0
+  enable      = var.enable && length(var.statements) > 0
+  policy_json = var.role == null ? data.aws_iam_policy_document.this[0].json : var.role
 }
 
 data "aws_iam_policy_document" "this" {
-  count = local.count
+  count = var.role == null ? local.count : 0
 
   dynamic "statement" {
     for_each = var.statements
@@ -21,7 +22,7 @@ resource "aws_iam_policy" "this" {
   count = local.count
 
   name_prefix = "grant-role-"
-  policy      = data.aws_iam_policy_document.this[0].json
+  policy      = local.policy_json
 }
 
 resource "aws_iam_role_policy_attachment" "this" {
--- aws/role-grant/variables.tf
+++ aws/role-grant/variables.tf
@@ -4,10 +4,19 @@ variable "enable" {
 }
 
 variable "role" {
-  type = string
+  description = "Name of the role"
+  type        = string
+}
+
+variable "policy" {
+  default     = null
+  description = "A full policy json to apply instead of the statements array. Use this when you need "
+  type        = string
 }
 
 variable "statements" {
+  default     = []
+  description = "A list of simply action/resource statements"
   type = list(object({
     actions   = list(string)
     resources = list(string)
